3ab0416b1f0443e60d88599e23763213ca20273e
ECD <edavidov01@larioja.edu.es>
2026-01-12 21:40:55 +0100

    Dia 2 final

---
+++ ejes5_3/Eje_5_3.java
@@ -0,0 +1,21 @@
+package ejes5_3;
+import java.util.Scanner;
+/**
+ *      crear las clase vehiculos y dos subclasescoche y bicicleta. un meno donde nos permita sumar y ver el resultado de los km
+ * 
+ * @author ECD
+ * @input      las opciones del menú y los km que se van a recorrer
+ * @output     los valores resultado de las opciones
+ * 
+ * 
+ */
+public class Eje_5_3 {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+       
+        
+        s.close();
+    }
+
+}
